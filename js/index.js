39c23874659e2e9a1e694a783fc2df7419fecbed
alex-B-Ly <abl12582@gmail.com>
2015-11-28 21:37:53 -0500

    Have to drop scroll effect idea because of Bootstrap limitation

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
- $(window).scroll(function() {
- 	var yPos = window.pageYOffset;
+ // $(window).scroll(function() {
+ // 	var yPos = window.pageYOffset;
 
- 	if(yPos >= 100){
- 		$('#nav-scroller').css('height', '30px');
- 		$('#nav-scroller a').css('font-size', '1.7em');
- 		$('#nav-scroller a').css('margin-top', '0');
- 	}else{
- 		$('#nav-scroller').css('height', '100px');
- 		$('#nav-scroller a').css('font-size', '2em');
- 		$('#nav-scroller a').css('margin-top', '20px');
- 	}
- });
+ // 	if(yPos >= 100){
+ // 		$('#nav-scroller').css('height', '30px');
+ // 		$('#nav-scroller a').css('font-size', '1.7em');
+ // 		$('#nav-scroller a').css('margin-top', '0');
+ // 	}else{
+ // 		$('#nav-scroller').css('height', '100px');
+ // 		$('#nav-scroller a').css('font-size', '2em');
+ // 		$('#nav-scroller a').css('margin-top', '20px');
+ // 	}
+ // });
  	
 
 });
