776cbaa56c84c4e022406c3674a09d263860bba6
alex-B-Ly <abl12582@gmail.com>
2015-11-29 00:25:29 -0500

    got the image change to work, need to add fade in

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
 	 	var yPos = window.pageYOffset;
 
 	 	if(yPos >= 100){
-	 		$('#nav-scroller').css('height', '50px');
+	 		$('#nav-scroller').css('height', '35px');
 	 		$('#nav-scroller a').css('font-size', '1.7em');
 	 		$('#nav-scroller a').css('margin-top', '0');
 			$('#nav-links a').css('font-size', '1.3em');
 	 		$('#nav-links a').css('margin-top', '0');
 	 	}else{
-	 		$('#nav-scroller').css('height', '100px');
+	 		$('#nav-scroller').css('height', '70px');
 	 		$('#nav-scroller a').css('font-size', '2.4em');
 	 		$('#nav-scroller a').css('margin-top', '20px');
 	 		$('#nav-links a').css('font-size', '1.5em');
@@ -23,7 +23,27 @@ $(document).ready(function(){
 	 });
  	}
 
+ 	// TIMED IMAGE CHANGE FUNCTION
+ 	function imageChanger(){
+ 		var imgList = ['underground', 'spain'];
+ 		var img = 0;
+
+ 		setInterval(function(){
+ 			if(img == imgList.length){
+ 				img = 0;
+ 				$('#head-images').css('background', 'url("img/'+imgList[img]+'.jpg") no-repeat');
+ 				$('#head-images').css('background-size', 'cover');
+ 			}else if(img < imgList.length){
+ 				$('#head-images').css('background', 'url("img/'+imgList[img]+'.jpg") no-repeat');
+	 			$('#head-images').css('background-size', 'cover');
+	 		}
+	 		img++;
+ 		}, 3000);
+
+ 	}
+
+
  	// FUNCTIONS CALLED
  	navScrollResize();
-
+ 	imageChanger();
 });
