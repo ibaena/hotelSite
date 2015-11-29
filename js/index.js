44c7ed1cb32e4e9e071ab04ad11624a9e97cbf81
alex-B-Ly <abl12582@gmail.com>
2015-11-28 22:43:39 -0500

    added nav just in case

--- js/index.js
+++ js/index.js
@@ -1,30 +1,29 @@
 $(document).ready(function(){
 
- $('[data-toggle="tooltip"]').tooltip(); 
 
+	// NAV RESIZE ON SCROLL FUNCTION
 
+	function navScrollResize(){
+	 $(window).scroll(function() {
+	 	var yPos = window.pageYOffset;
 
+	 	if(yPos >= 100){
+	 		$('#nav-scroller').css('height', '50px');
+	 		$('#nav-scroller a').css('font-size', '1.7em');
+	 		$('#nav-scroller a').css('margin-top', '0');
+			$('#nav-links a').css('font-size', '1.3em');
+	 		$('#nav-links a').css('margin-top', '0');
+	 	}else{
+	 		$('#nav-scroller').css('height', '100px');
+	 		$('#nav-scroller a').css('font-size', '2.4em');
+	 		$('#nav-scroller a').css('margin-top', '20px');
+	 		$('#nav-links a').css('font-size', '1.5em');
+	 		$('#nav-links a').css('margin-top', '10px');
+	 	}
+	 });
+ 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 	// FUNCTIONS CALLED
+ 	navScrollResize();
 
 });
