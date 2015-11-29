afadc6e20c88773b9c1e293b055ee2bc51457e58
alex-B-Ly <abl12582@gmail.com>
2015-11-28 19:31:50 -0500

    added some wizardry to navbar

--- js/index.js
+++ js/index.js
@@ -5,8 +5,12 @@ $(document).ready(function(){
 
  	if(yPos >= 100){
  		$('#nav-scroller').css('height', '30px');
+ 		$('#nav-scroller a').css('font-size', '1.7em');
+ 		$('#nav-scroller a').css('margin-top', '0');
  	}else{
  		$('#nav-scroller').css('height', '100px');
+ 		$('#nav-scroller a').css('font-size', '2em');
+ 		$('#nav-scroller a').css('margin-top', '20px');
  	}
  });
  	
