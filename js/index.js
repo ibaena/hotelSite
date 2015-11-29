d3df6bd1096433c9facd043909bf18681722580b
alex-B-Ly <abl12582@gmail.com>
2015-11-28 19:06:50 -0500

    added simple navbar with shrinking effect on scroll, have to make it responsive

--- js/index.js
+++ js/index.js
@@ -1,30 +1,14 @@
 $(document).ready(function(){
 
- $('[data-toggle="tooltip"]').tooltip(); 
+ $(window).scroll(function() {
+ 	var yPos = window.pageYOffset;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 	if(yPos >= 100){
+ 		$('#nav-scroller').css('height', '30px');
+ 	}else{
+ 		$('#nav-scroller').css('height', '100px');
+ 	}
+ });
+ 	
 
 });
