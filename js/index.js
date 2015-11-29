fddbf6f9f951c5cde3a04e5f8b32b485664ce5b1
alex-B-Ly <abl12582@gmail.com>
2015-11-28 22:24:16 -0500

    fixed navbar and retained some wizardry.  Bootstrap CSS does not like me very much.

--- js/index.js
+++ js/index.js
@@ -4,13 +4,17 @@ $(document).ready(function(){
  	var yPos = window.pageYOffset;
 
  	if(yPos >= 100){
- 		$('#nav-scroller').css('height', '30px');
+ 		$('#nav-scroller').css('height', '50px');
  		$('#nav-scroller a').css('font-size', '1.7em');
  		$('#nav-scroller a').css('margin-top', '0');
+		$('#nav-links a').css('font-size', '1.3em');
+ 		$('#nav-links a').css('margin-top', '0');
  	}else{
  		$('#nav-scroller').css('height', '100px');
  		$('#nav-scroller a').css('font-size', '2em');
  		$('#nav-scroller a').css('margin-top', '20px');
+ 		$('#nav-links a').css('font-size', '1.5em');
+ 		$('#nav-links a').css('margin-top', '10px');
  	}
  });
  	
